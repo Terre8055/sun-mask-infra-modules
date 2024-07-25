e5c25f46e766e2e8509c1e3b0597b5c577d087da
Michael Appiah Dankwah <michaelappiah2018@icloud.com>
2024-07-25 15:43:38 +0000

    Change instance type to 10 t3.xlarge for running Hadoop
    Signed-off-by: Michael Appiah Dankwah <michaelappiah2018@icloud.com>

--- variables.tf
+++ variables.tf
@@ -59,7 +59,7 @@ variable "availability_zones" {
 variable "ec2_count" {
   description = "Number of provisioned servers"
   type = number
-  default = 6
+  default = 10
 }
 
 # variable "ec2_subnetID" {
